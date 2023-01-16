3baf8d29328748147f347964a3f43508daccf1c3
Plamen Georgiev <p.olegov@yahoo.com>
2023-01-16 08:40:01 +0200

    Add article to body

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,17 @@
 import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
-  // This block will be executed once the page is loaded and ready
+    // This block will be executed once the page is loaded and ready
 
-  const button = document.querySelector(".button");
-  button.addEventListener("click", () => {
-    alert("💣");
-  });
+    const button = document.querySelector(".button");
+    button.addEventListener("click", () => {
+        alert("💣");
+    });
+
+    const body = document.querySelector("body");
+    body.addEventListener("click", () => {
+        for (let i = 1; i <= 5; i++) {
+            body.innerHTML = '<article>This is article - ' + i + '</article>';
+        }
+    });
 });
